dd0c5eb391aa2722f2f147897da85d1ae142ba1f
Сергей Носачёв <crackkc@mail.ru>
2026-02-13 11:52:21 +0300

    add fexes to commit

--- model.py
+++ model.py
@@ -23,7 +23,7 @@ from tensorflow.keras.layers import (
 
 from tensorflow.keras import Model
 
-from utils import yolo_boxes, broadcast_iou, nonMaximumSuppression
+from utils import yolo_boxes, broadcast_iou, nonMaximumSuppression, intersectionOverUnion
 
 
 # Архитектура модели YOLOv3
@@ -37,9 +37,9 @@ def DBL(x, filters, kernel, strides=1, batch_norm=True):
         x = ZeroPadding2D(((1, 0), (1, 0)))(x)
         padding = 'valid'
     x = Conv2D(filters=filters, kernel_size=kernel,
-               strides=strides, padding=padding,
-               use_bias=not batch_norm,  # используем смещение, если нет нормализации
-               kernel_regularizer=l2(0.0005))(x)
+              strides=strides, padding=padding,
+              use_bias=not batch_norm,  # используем смещение, если нет нормализации
+              kernel_regularizer=l2(0.0005))(x)
     if batch_norm:
         x = BatchNormalization(epsilon=0.001)(x)
         x = LeakyReLU(negative_slope=0.1)(x)
@@ -48,17 +48,17 @@ def DBL(x, filters, kernel, strides=1, batch_norm=True):
 
 # Блок **ResUnit**
 def ResUnit(x, filters):
-    skip_connection = x
+    skip_connection  = x
     x = DBL(x, filters // 2, 1)
     x = DBL(x, filters, 3)
-    x = Add()([skip_connection, x])
+    x = Add()([skip_connection , x])
     return x
 
 
 # Блок **ResN**
 def ResN(x, filters, blocks):
     x = DBL(x, filters, kernel=3, strides=2)
-    for _ in repeat(None, blocks):  # цикл по n-блокам (подход с repeat позволяет экономить память)
+    for _ in repeat(None, blocks): # цикл по n-блокам (подход с repeat позволяет экономить память)
         x = ResUnit(x, filters)
     return x
 
@@ -78,7 +78,7 @@ def Darknet(name=None):
 # Головы YOLOv3
 def YoloHead(filters, name=None):
     def layer(x_in):
-        if isinstance(x_in, tuple):  #
+        if isinstance(x_in, tuple): #
             inputs = Input(x_in[0].shape[1:]), Input(x_in[1].shape[1:])
             x, x_skip = inputs
 
@@ -94,27 +94,26 @@ def YoloHead(filters, name=None):
         x = DBL(x, filters * 2, 3)
         x = DBL(x, filters, 1)
         return Model(inputs, x, name=name)(x_in)
-
     return layer
-
 
 def YoloHeadOutput(filters, anchors, classes, name=None):
     def layer(x_in):
+
         x = inputs = Input(x_in.shape[1:])
         x = DBL(x, filters * 2, 3)
         x = DBL(x, anchors * (classes + 5), 1, batch_norm=False)
         x = Lambda(lambda x: tf.reshape(x, (-1, tf.shape(x)[1], tf.shape(x)[2],
-                                            anchors, classes + 5)))(x)
+                                        anchors, classes + 5)))(x)
         return tf.keras.Model(inputs, x, name=name)(x_in)
-
     return layer
 
 
 # модель
 def YoloV3(size=None, channels=3, anchors=settings.YOLO_ANCHORS,
-           masks=settings.YOLO_ANCHOR_MASKS, classes=80, training=False):
+            masks=settings.YOLO_ANCHOR_MASKS, classes=80, training=False):
     # Вход
     x = inputs = Input([size, size, channels])
+
     # Модель Darknet с тремя выходами
     Route_1, Route_2, Route_3 = Darknet(name='yolo_darknet')(x)
 
@@ -128,27 +127,31 @@ def YoloV3(size=None, channels=3, anchors=settings.YOLO_ANCHORS,
     x = YoloHead(128, name='yolo_head_3')((x, Route_1))
     output_2 = YoloHeadOutput(128, len(masks[2]), classes, name='yolo_output_3')(x)
 
-    if training:  # заложим модель для обучения (понадобиться в домашней работе)
+
+    if training: # заложим модель для обучения (понадобиться в домашней работе)
         return Model(inputs, (output_0, output_1, output_2), name='yolov3')
 
     # Перегоняем предсказанные якорные рамки в boundary box для трех выходов
     boxes_0 = Lambda(lambda x: yolo_boxes(x, anchors[masks[0]], classes),
-                     name='yolo_boxes_0')(output_0)
+                  name='yolo_boxes_0')(output_0)
     boxes_1 = Lambda(lambda x: yolo_boxes(x, anchors[masks[1]], classes),
-                     name='yolo_boxes_1')(output_1)
+                  name='yolo_boxes_1')(output_1)
     boxes_2 = Lambda(lambda x: yolo_boxes(x, anchors[masks[2]], classes),
-                     name='yolo_boxes_2')(output_2)
+                  name='yolo_boxes_2')(output_2)
 
     # Отсекаем все лишнее алгоритмом NMS
     outputs = Lambda(lambda x: nonMaximumSuppression(x, anchors, masks, classes),
-                     name='nonMaximumSuppression')((boxes_0[:3], boxes_1[:3], boxes_2[:3]))
+                  name='nonMaximumSuppression')((boxes_0[:3], boxes_1[:3], boxes_2[:3]))
 
-    return Model(inputs, outputs, name='yolov3')  # возвращаем модель для инференса
+    return Model(inputs, outputs, name='yolov3') # возвращаем модель для инференса
+
+# The function to load weights from pretrained model
 
 
 # Функция потерь
 def YoloLoss(anchors, classes=80, ignore_thresh=0.5):
     def yolo_loss(y_true, y_pred):
+
         # 1. Преобразуем все предсказанные выходы
         # y_pred: (batch_size, grid, grid, anchors, (x, y, w, h, obj, ...cls))
         # с функцией yolo_boxes мы уже знакомы
@@ -163,6 +166,7 @@ def YoloLoss(anchors, classes=80, ignore_thresh=0.5):
             y_true, (4, 1, 1), axis=-1)
         true_xy = (true_box[..., 0:2] + true_box[..., 2:4]) / 2
         true_wh = true_box[..., 2:4] - true_box[..., 0:2]
+
         # Чем меньше рамка, тем больше ошибка
         box_loss_scale = 2 - true_wh[..., 0] * true_wh[..., 1]
 
@@ -171,39 +175,31 @@ def YoloLoss(anchors, classes=80, ignore_thresh=0.5):
         grid = tf.meshgrid(tf.range(grid_size), tf.range(grid_size))
         grid = tf.expand_dims(tf.stack(grid, axis=-1), axis=2)
         true_xy = true_xy * tf.cast(grid_size, tf.float32) - \
-                  tf.cast(grid, tf.float32)
+            tf.cast(grid, tf.float32)
         true_wh = tf.math.log(true_wh / anchors)
         true_wh = tf.where(tf.math.is_inf(true_wh),
-                           tf.zeros_like(true_wh), true_wh)
+                      tf.zeros_like(true_wh), true_wh)
 
         # 4. Вычисляем ignore_mask
         obj_mask = tf.squeeze(true_obj, -1)
         true_box_flat = tf.boolean_mask(true_box, tf.cast(obj_mask, tf.bool))
-
-        # best_iou = tf.reduce_max(intersectionOverUnion(
-        #    pred_box, true_box_flat), axis=-1)
-
-        best_iou = tf.map_fn(
-            lambda x: tf.reduce_max(broadcast_iou(x[0], tf.boolean_mask(
-                x[1], tf.cast(x[2], tf.bool))), axis=-1),
-            (pred_box, true_box, obj_mask),
-            tf.float32)
+        best_iou = tf.reduce_max(intersectionOverUnion(
+            pred_box, true_box_flat), axis=-1)
         ignore_mask = tf.cast(best_iou < ignore_thresh, tf.float32)
 
         # 5. Вычисляем все функции ошибок
         xy_loss = obj_mask * box_loss_scale * \
-                  tf.reduce_sum(tf.square(true_xy - pred_xy), axis=-1)
+            tf.reduce_sum(tf.square(true_xy - pred_xy), axis=-1)
         wh_loss = obj_mask * box_loss_scale * \
-                  tf.reduce_sum(tf.square(true_wh - pred_wh), axis=-1)
+            tf.reduce_sum(tf.square(true_wh - pred_wh), axis=-1)
 
         obj_loss = binary_crossentropy(true_obj, pred_obj)
-        # ignore_mask = tf.expand_dims(ignore_mask, -1) # edit
         obj_loss = obj_mask * obj_loss + \
-                   (1 - obj_mask) * ignore_mask * obj_loss
+            (1 - obj_mask) * ignore_mask * obj_loss
 
         # Можно использовать как binary_crossentropy, так и sparse_categorical_crossentropy
-        spars = sparse_categorical_crossentropy(true_class_idx, pred_class)
-        class_loss = obj_mask * spars
+        class_loss = obj_mask * sparse_categorical_crossentropy(
+            true_class_idx, pred_class)
 
         # 6. Все функции ошибок суммируются по осям для каждого пакета (batch, gridx, gridy, anchors) => (batch, 1)
         xy_loss = tf.reduce_sum(xy_loss, axis=(1, 2, 3))
@@ -212,7 +208,5 @@ def YoloLoss(anchors, classes=80, ignore_thresh=0.5):
         class_loss = tf.reduce_sum(class_loss, axis=(1, 2, 3))
 
         # Общая суммарная ошибка
-        summ = xy_loss + wh_loss + obj_loss + class_loss
         return xy_loss + wh_loss + obj_loss + class_loss
-
     return yolo_loss
